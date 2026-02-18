6f746d6d5cc42656d3d83d66772877d128611acc
Raman Kumar <ramankr7321@gmail.com>
2026-02-18 15:10:56 +0530

    Insertion of node at head.

---
+++ Insertion_at_head.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+struct node{
+    int data;
+    node *next;
+};
+
+void printlist(node *n){
+    while(n != NULL){
+        cout << n ->data <<endl;
+        n = n -> next;
+    }
+}
+
+node *push(node *head , int newData){
+    node *newNode = new node();
+    newNode -> data = newData;
+    newNode -> next = head;
+    head = newNode;
+    return head;
+}
+
+int main()
+{
+    node *head = NULL;
+    head = push(head ,5);
+    head = push(head ,7);
+
+    printlist(head);
+}
